0745476a4913101d9182fb2a4feb81a29b5f2fe6
Jensen <118268364+Jensen-3000@users.noreply.github.com>
2023-07-31 14:56:06 +0200

    add: lots of things; todo: fix css

---
+++ QuizAppWeb/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace QuizAppWeb.Models
+{
+    public class Question
+    {
+        [JsonPropertyName("QuestionText")]
+        public string QuestionText { get; set; } = string.Empty;
+
+        [JsonPropertyName("Options")]
+        public IEnumerable<string> QuestionOptions { get; set; } = new List<string>();
+
+        [JsonPropertyName("CorrectAnswerIndex")]
+        public int QuestionCorrectAnswerIndex { get; set; }
+
+        [JsonPropertyName("CodeSnippet")]
+        public string QuestionCodeSnippet { get; set; } = string.Empty;
+    }
+}
---
+++ QuizAppWeb/Pages/Quizzes/QuizCardBase.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using QuizAppWeb.Models;
+using System.Text.Json;
+
+namespace QuizAppWeb.Pages.Quizzes
+{
+    public class QuizCardBase : ComponentBase
+    {
+        public List<Question> Questions { get; set; } = new List<Question>();
+
+        protected int questionIndex = 0;
+        protected int score = 0;
+
+        [Inject]
+        protected HttpClient httpClient { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadQuestions();
+            await base.OnInitializedAsync();
+        }
+
+        protected void OptionSelected(string option)
+        {
+            if (option == Questions[questionIndex].QuestionOptions.ElementAt(Questions[questionIndex].QuestionCorrectAnswerIndex))
+            {
+                score++;
+            }
+            questionIndex++;
+        }
+
+        protected void RestartQuiz()
+        {
+            score = 0;
+            questionIndex = 0;
+        }
+
+        private async Task LoadQuestions()
+        {
+            string jsonData = await httpClient.GetStringAsync("/Assets/prog_quiz.json");
+            Questions = JsonSerializer.Deserialize<List<Question>>(jsonData);
+        }
+    }
+}
